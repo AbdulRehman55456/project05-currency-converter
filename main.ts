6e127f173dd7bf5d9ed44c223565bac15d5cc287
AbdulRehman55456 <siontoji@gmail.com>
2024-04-19 03:30:24 -0700

    first commit

---
+++ main.ts
@@ -0,0 +1,38 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+
+const currency : any = {
+  USD : 1,
+ EUR : 0.91,
+ GBP : 0.72,
+ INR : 70,
+ PKR :280,
+};
+
+let user_answer = await inquirer.prompt([{
+    name:"from",
+    message: "enter from currency",
+    type: "list",
+    choices:["USD","EUR","GBP","INR","PKR"]
+},{
+    name:"to",
+    message: "enter to currency",
+    type: "list",
+    choices:["USD","EUR","GBP","INR","PKR"]
+},{
+  name:"amount",
+  message:"enter your amount",
+  type:"number"
+}
+]
+)
+let fromamount =currency[user_answer.from];
+let toamount = currency[user_answer.to];
+let amount = user_answer.amount;
+let baseamount = amount / fromamount ;
+let covertedamount = baseamount * toamount;
+console.log(covertedamount);
+//console.log(fromamount);
+//console.log(toamount);
+//console.log(amount);
